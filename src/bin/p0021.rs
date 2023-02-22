0352b812a96c61607e82db2e05850ed1d7c9964e
redanian <>
2023-02-22 14:59:40 +0100

    add p21 solution

---
+++ src/bin/p0021.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let result = (2..10000)
+        .filter(|a| {
+            let b = a.divisors().into_iter().sum::<u32>();
+            *a != b && *a == b.divisors().into_iter().sum::<u32>()
+        })
+        .sum::<u32>();
+
+    println!("Problem 21 solution: {}", result);
+}
+
+trait HasDivisors {
+    fn divisors(&self) -> Vec<Self> where Self: Sized;
+}
+
+impl HasDivisors for u32 {
+    fn divisors(&self) -> Vec<u32> {
+        let n = *self;
+        match n {
+            0 | 1 => vec![],
+            _ => (2..((n as f64).sqrt().ceil() as u32))
+                .fold(vec![1], |acc, i| if n % i == 0 { [acc, vec![i, n / i]].concat() } else { acc })
+        }
+    }
+}
